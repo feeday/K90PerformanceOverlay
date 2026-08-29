3b9b503e6c992eb6228fa1e6cbc381fca296676d
PPT <realfake.top@gmail.com>
2026-08-29 12:29:21 +0800

    Bump cooler protocol integration to 5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ppt.k90monitor"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "5.0"
+        versionCode = 7
+        versionName = "5.1"
     }
 
     buildTypes {
